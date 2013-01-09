6445b8dad0e043dbdc717dd517a9ba3ad6f596dc
fyhack <fyhack@qq.com>
2013-01-09 14:38:23 +0800

    停靠控件(dockedlistview)

--- ControlLibrary/src/com/fyhack/controllibrary/MainActivity.java
+++ ControlLibrary/src/com/fyhack/controllibrary/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
-import com.fyhack.R;
+import com.fyhack.dockedlistview.DockedListViewActivity;
 
 public class MainActivity extends Activity implements OnClickListener{
 	private LinearLayout mainLayout;
@@ -48,7 +48,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		switch(v.getId()){
 			case DockedListViewId :
-				this.startActivity(new Intent(this,org.fyhack.dockedlistview.MainActivity.class));
+				this.startActivity(new Intent(this,DockedListViewActivity.class));
 				break;
 		}
 	}
