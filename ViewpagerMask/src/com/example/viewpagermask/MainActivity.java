38d17a70ab9619303eb7d5a8ebef3d4493986ab2
elc_simayi <elc_simayi@elc_simayi-PC>
2014-02-11 18:31:17 +0800

    add : 增加旋转效果

--- ViewpagerMask/src/com/example/viewpagermask/MainActivity.java
+++ ViewpagerMask/src/com/example/viewpagermask/MainActivity.java
@@ -3,28 +3,36 @@ package com.example.viewpagermask;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends FragmentActivity {
-    private ViewPager viewPager ;
-    private List<Fragment> fragments ;
+    private ViewPager viewPager;
+    private List<Fragment> fragments;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        startAnim();
     }
-    
-    private void initViews(){
+
+    private void initViews() {
         viewPager = (ViewPager) findViewById(R.id.guide_viewPager);
+        btn = (Button) findViewById(R.id.btn_guide_enter);
         fragments = new ArrayList<Fragment>();
         FragmentOne one = new FragmentOne();
         FragmentTwo two = new FragmentTwo();
@@ -32,7 +40,80 @@ public class MainActivity extends FragmentActivity {
         fragments.add(two);
         ViewAdapter adapter = new ViewAdapter(getSupportFragmentManager(), fragments);
         viewPager.setAdapter(adapter);
-        
+        viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int arg0) {
+                changeDot(arg0);
+                if (arg0 == viewPager.getAdapter().getCount()-1) {
+                    btn.setVisibility(View.VISIBLE);
+                }else{
+                    btn.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+
+        changeDot(0);
+    }
+
+    private void changeDot(int page) {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.ll_guide_point_icon);
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            if (i == page) {
+                layout.getChildAt(i).setBackgroundResource(R.drawable.dot_guide_s);
+            } else {
+                layout.getChildAt(i).setBackgroundResource(R.drawable.dot_guide_n);
+            }
+        }
+    }
+    
+    private void startAnim(){
+        startAnimHorseView(new OnAfterStart() {
+            
+            @Override
+            public void todo() {
+                startAnimTopView();                
+            }
+        });
+    }
+    
+    private void startAnimHorseView(final OnAfterStart afterStart){
+        final View horse = findViewById(R.id.iv_guide_horse);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.guide_horse_set);
+        anim.setAnimationListener(new AnimationListener() {
+                public void onAnimationStart(Animation arg0) {
+                }
+                public void onAnimationRepeat(Animation arg0) {
+                }
+                public void onAnimationEnd(Animation arg0) {
+                    horse.setVisibility(View.INVISIBLE);
+                    afterStart.todo();
+                }
+        });
+        horse.startAnimation(anim);
+    }
+    
+    private void startAnimTopView(){
+        final View top = findViewById(R.id.fl_guide_top);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.guide_horse_view_trans);
+        anim.setAnimationListener(new AnimationListener() {
+                public void onAnimationStart(Animation arg0) {
+                }
+                public void onAnimationRepeat(Animation arg0) {
+                }
+                public void onAnimationEnd(Animation arg0) {
+                    top.setVisibility(View.INVISIBLE);
+                }
+        });
+        top.startAnimation(anim);
     }
 
     private class ViewAdapter extends FragmentPagerAdapter {
@@ -59,5 +140,9 @@ public class MainActivity extends FragmentActivity {
             return list.size();
         }
 
+    }
+    
+    private interface OnAfterStart{
+        public void todo();
     }
 }
